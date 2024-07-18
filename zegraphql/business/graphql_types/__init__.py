cae9dd76dae2cd8c05b47a8735aa64c8f44bab51
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2024-07-18 11:24:58 +0300

    reomve old sperated files for types

--- zegraphql/business/graphql_types/__init__.py
+++
@@ -1,4 +0,0 @@
-from .documents import CreateDocumentInput, UpdateDocumentInput
-from .industries import CreateIndustryInput, UpdateIndustryInput
-from .list_types import DocumentType, IndustryType
-
